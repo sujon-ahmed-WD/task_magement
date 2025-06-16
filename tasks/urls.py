9c622f7e7207656ec39c25138b911d052cb417e1
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-06-16 08:08:25 +0600

    Url bug fixed

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from tasks.views import show_task,show_specific_task
 
 
 urlpatterns = [
-    path("show_task/",show_task)
+    path("show_task/",show_task),
     path("show_task/<int:id>",show_specific_task)
     
 ]
